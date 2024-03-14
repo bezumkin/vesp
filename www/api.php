dafd17863a4f8240d0ec835d4b3577c89f95c71c
bezumkin <bezumkin@yandex.ru>
2024-03-14 11:49:57 +0700

    feat: update vesp/core

--- www/api.php
+++ www/api.php
@@ -2,7 +2,10 @@
 
 require dirname(__DIR__) . '/core/bootstrap.php';
 
-$app = DI\Bridge\Slim\Bridge::create();
+$container = new DI\Container();
+$container->set(Illuminate\Database\Capsule\Manager::class, new Vesp\Services\Eloquent());
+
+$app = DI\Bridge\Slim\Bridge::create($container);
 $app->add(App\Middlewares\Auth::class);
 $app->add(new RKA\Middleware\IpAddress());
 $app->addBodyParsingMiddleware();
